817dda0fbb405328a81e3659e2f91b00366b930a
Marcelo Martin <marcemartin8@hotmail.com>
2026-06-25 15:50:07 -0300

    Update carga.py

--- Descargas/carga.py
+++ Descargas/carga.py
@@ -8,17 +8,14 @@ from dotenv import load_dotenv
 
 def guardar_archivo():
     # Función para exportar archivo en formato .CSV al directorio local.
-    try:
-        ingreso_ruta = input('Copie y pegue el directorio donde guardar el archivo .CSV: ')
-        ruta_archivo = f"{ingreso_ruta}"
-        nombre_csv_salida = "comisiones_consolidadas.csv"
-        ruta_salida_completa = os.path.join(ruta_archivo, nombre_csv_salida)
-        df_final_limpio.to_csv(ruta_salida_completa, index=False, encoding='utf-8')
-        mensaje = print('✅ ¡Archivo guardado exitosamente!')
-        
-    except Exception as e:
-        mensaje = print('⚠️ No se pudo guardar el archivo por el siguiente error:')
-        print(type(e))
+    
+    ingreso_ruta = input('Copie y pegue el directorio donde guardar el archivo .CSV: ')
+    ruta_archivo = f"{ingreso_ruta}"
+    nombre_csv_salida = "comisiones_consolidadas.csv"
+    ruta_salida_completa = os.path.join(ruta_archivo, nombre_csv_salida)
+    df_final_limpio.to_csv(ruta_salida_completa, index=False, encoding='utf-8')
+    mensaje = print('✅ ¡Archivo guardado exitosamente!')
+    
     return mensaje
 
 def recuperar_script_sql():
